03628f32983cf0fc07813a9014e3dfed64691f40
Brad Sheese <bsheese@iwu.edu>
2023-08-28 09:46:21 -0500

    updated template

--- content/grading.py
+++ content/grading.py
@@ -28,13 +28,35 @@ work_dic = {
         responses before class is critical, and so responses will not be accepted after the assigned deadline. Late 
         or missing reading response submissions receive zero points.''',
 
-    'Exercises':
-        '''Every week we will have assigned exercises that students are responsible for completing independently 
-        during this time. These exercises are designed to refine and expand your understanding of the course material 
-        and are essential for success in the class. Exercises will be posted on Moodle and aredue at set times as 
-        indicated on Moodle. Exercise submissions with missing or broken revision histories will receive zero points. 
-        Copy and pasting from others notebooks is prohibited. Exercise submissions with any form of directly copied 
-        code receive will zero points. Late or missing exercise submissions receive zero points.''',
+    'Exercises and Assignments':
+        '''Every week we will have assigned work that students are responsible for completing independently. 
+        Exercises and assignments are designed to refine and expand your understanding of the course material 
+        and are essential for success in the class. Exercises will be posted on Moodle and are due at set times as 
+        indicated on Moodle. <br><p>
+        
+        Please note the following: 
+        <li>Note book submission must be free from errors and run properly. Students should selected 'Restart and run 
+        all' prior to submission to verify that everything is working. Notebooks with errors will receive zero 
+        points.</li> 
+        <li>Submissions with missing or broken revision histories will receive zero points.</li> 
+        <li>Copy and pasting from other notebooks is prohibited. Exercise submissions with any form of directly copied 
+        code receive will zero points.</li>
+        <li>AI generated code is prohibited (see course AI policy). Submission of A.I. generated code will result 
+        in zero points and a referral for academic dishonesty.</li> 
+        <li>Late or missing exercise submissions receive zero points. Students should be submitting work well in 
+        advance of the deadline.</li>
+        <li>Assignments will get longer and more complicated as the semester progresses because we have more coding
+        tools to use to solve problems.</li> 
+        <li>Assignments are graded for completeness and correctness. To earn credit, your assignment must be fully
+         complete and correct.</li> 
+        <li>The questions in each assignment/exercise typically build up to the final question.
+         You need to reach the final questions and complete it to receive credit.</li> 
+        <li>We are happy to provide some pointers/suggestions as you work on your assignment. But, we are not 
+        going to give you the answers or do the work for you.  Please make effective use of our office hours 
+        and class time.</li>   
+        <li>Successful students will often have the week's work done before Friday so they can get 
+        help they need along the way.</li>  
+        ''',
 
     'Quizzes and Exams':
         '''Quizzes and exams will cover all material until that point in the semester, but will primarily focus on 
@@ -44,6 +66,13 @@ work_dic = {
     'Projects/Presentations':
         '''Any required projects or presentations will be announced in class and detailed information 
         will be provided on Moodle.''',
+
+    'CodeHelp Usage':
+        '''At the end of the semester students will be graded on the quantity and quality of CodeHelp submissions.
+        Students are expected to submit a minimum of 40 queries during the course of the semester. To be counted as 
+        a distinct query, queries must not duplicate previous queries (asking the same question again does not count 
+        as a new query), must be relevant to the class, and must show evidence of the student progressing through course 
+         related problems.''',
 
     'Grade for the Course':
         '''Course grades will be calculated at the end of the term by dividing the points earned by the total number of 
